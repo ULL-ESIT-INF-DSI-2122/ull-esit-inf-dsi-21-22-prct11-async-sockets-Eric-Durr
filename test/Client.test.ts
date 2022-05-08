942e7094e477a3152689e88692a6ace2f2a31f10
Eric DS <eric.d.s.info@gmail.com>
2022-05-08 19:40:13 +0100

    test: modification for remote case :gear:

--- test/Client.test.ts
+++ test/Client.test.ts
@@ -18,64 +18,89 @@ describe('Client with no server up', () => {
 
 describe('Client with server up', () => {
   const server: Server = new Server(8989);
-  const client: Client = new Client(8989);
   server.runServer();
   after(() => {
     server.closeServer();
-    client.closeClient();
   });
 
   it('Client wants to do something but user des not exist', (done) => {
+    const client: Client = new Client(8989);
     client.once('errormsg', (message: string) => {
       expect(message).to.be.eq('ERROR: user random does not exists');
+      client.closeClient();
       done();
     });
     client.listNotes('random');
   });
 
   it('Client wants to retrieve a notes list for a user with no notes', (done) => {
+    const client: Client = new Client(8989);
     client.once('info', (message: string) => {
       if (message) {
         expect(message).to.be.eq('No notes found for Eric ...');
       }
+      client.closeClient();
+      done();
+    });
+
+    client.once('errormsg', (message: string) => {
+      if (message) {
+        expect(message).to.be.eq('ERROR: user Eric does not exists');
+      }
+      client.closeClient();
       done();
     });
     client.listNotes('Eric');
   });
 
   it('Client wants to retrieve a notes list for a user with notes', (done) => {
+    const client: Client = new Client(8989);
     client.once('list', (message: string) => {
       const notes: ColoredMessage[] = JSON.parse(message);
       expect(notes
         .map((note: ColoredMessage) => note.content)
         .join(', '))
         .to.be.eq('New note (4), New note (5), New note (6), New note, my new note 2, my new note, my red note');
+      client.closeClient();
       done();
     });
     client.listNotes('Pablo');
   });
 
   it('Client wants to read a note but user has no notes', (done) => {
+    const client: Client = new Client(8989);
     client.once('info', (message: string) => {
       if (message) {
         expect(message).to.be.eq('No notes found for Eric ...');
       }
+      client.closeClient();
+      done();
+    });
+    client.once('errormsg', (message: string) => {
+      if (message) {
+        expect(message).to.be.eq('ERROR: user Eric does not exists');
+      }
+      client.closeClient();
       done();
     });
     client.readNote('Eric', 'No existing');
   });
 
   it('Client wants to read a note but fails', (done) => {
+    const client: Client = new Client(8989);
     client.once('errormsg', (message: string) => {
       expect(message).to.be.eq('ERROR: Note with title No existing doesn\'t exist');
+      client.closeClient();
       done();
     });
     client.readNote('Pablo', 'No existing');
   });
 
   it('Client wants to read a note but succeeds', (done) => {
+    const client: Client = new Client(8989);
     client.once('blue', (message: string) => {
       expect(message).to.be.eq('New note (6)\n────────────\nnew body\n');
+      client.closeClient();
       done();
     });
     client.readNote('Pablo', 'New note (6)');
